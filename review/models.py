8a194d0108ad68dc582ed9c1af74b9d1a9782f50
Mabacore101 <valentinozhuo2@gmail.com>
2025-04-03 10:59:02 +0700

    Added validators for star_rating.

--- review/models.py
+++ review/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 from finance.models import Bill
-
 
 # Create your models here.
 class Review(models.Model):
     bill = models.OneToOneField(Bill, on_delete=models.CASCADE)
     comment = models.TextField(max_length=1000)
-    star_rating = models.DecimalField(max_digits=1, decimal_places=0)
+    star_rating = models.DecimalField(max_digits=1, decimal_places=0, validators=[MinValueValidator(1), MaxValueValidator(5)])
