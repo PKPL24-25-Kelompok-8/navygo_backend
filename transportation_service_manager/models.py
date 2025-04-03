884fba11f3d2e9294ac398514f245f45c5d72668
Danniel-Ang <danniel@ui.ac.id>
2025-04-03 11:20:13 +0700

    [Feat] Models redesign and adding couple of Model

--- transportation_service_manager/models.py
+++ transportation_service_manager/models.py
@@ -28,3 +28,23 @@ class Vehicle(models.Model):
     capacity = models.IntegerField()
     current_port = models.ForeignKey(Port, on_delete=models.SET_NULL, null=True, blank=True, related_name="vehicles")
 
+class PortVisit(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    current_vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name="portvisit")
+    current_port = models.ForeignKey(Port, on_delete=models.CASCADE, related_name="portvisit")
+    port_destination = models.ForeignKey(Port, on_delete=models.CASCADE, related_name="portvisit")
+    arrival_time = models.DateTimeField()
+    departure_time = models.DateTimeField()
+
+class TransportationService(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    port_visit = models.ForeignKey(PortVisit, on_delete=models.CASCADE, related_name="transportation_service")
+    status = models.ForeignKey('StatusTransportationService', on_delete=models.CASCADE, related_name="transportation_service")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class StatusTransportationService(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
