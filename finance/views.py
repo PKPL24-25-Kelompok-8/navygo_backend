c560417b42aff5c8affcc07520675558639bc7b5
lantry-glitch <allan.kwek.18@gmail.com>
2025-04-04 11:41:22 +0700

    fix settings.py non installed app

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import TrNavypay
+from .serializers import TopUpSerializer
 
-# Create your views here.
+class TopUpAPIView(generics.CreateAPIView):
+    queryset = TrNavypay.objects.all()
+    serializer_class = TopUpSerializer
